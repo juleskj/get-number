6d6d380a98173d7ece5419c724e9f1d3386048b6
Jules KJ <juljen2730@gmail.com>
2024-09-17 11:14:41 +0200

    get number

--- index.js
+++ index.js
@@ -1,22 +1,9 @@
-let max = 100;
-let rand = Math.round(Math.random() * 100);
-// const button = document.querySelector("#submitButton");
-// console.log(rand);
-function guessNumber() {
-  const inputNumber = document.getElementById("guessBox").value;
-  // her henter jeg valuen fra inputfæltet from en stirng og
-  //den bliver lavet om til et nummer
-  const number = parseFloat(inputNumber);
+let max = 10;
 
-  if (number == rand) {
-    document.getElementById("result").textContent =
-      "Yay you have guessed the number!";
-    document.querySelector("body").classList.add("linear");
-  } else if (number < rand) {
-    document.getElementById("result").textContent = "higer!";
-  } else if (number > rand) {
-    document.getElementById("result").textContent = "Lower!";
-  }
+const Number = document.querySelector("#number");
 
-  console.log(number);
+Number.addEventListener("mousedown", newNumber);
+
+function newNumber() {
+  Number.innerHTML = Math.round(Math.random() * max);
 }
